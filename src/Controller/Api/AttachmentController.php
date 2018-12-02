173f8c21ec74e1509b6df930c884412890aaacee
konshensx16 <konshensx16@gmail.com>
2018-12-02 14:59:22 +0100

    uploading the attachments vie the AttachmentManager, currenctly just uploading the file not saving to the post nor dbm NOTHING , just uploading

--- src/Controller/Api/AttachmentController.php
+++ src/Controller/Api/AttachmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api;
 
+use App\Services\AttachmentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,16 +15,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class AttachmentController extends AbstractController
 {
     /**
+     * @var AttachmentManager
+     */
+    private $attachmentManager;
+
+    public function __construct(AttachmentManager $attachmentManager)
+    {
+        $this->attachmentManager = $attachmentManager;
+    }
+
+    /**
      * @Route("/postimage", name="postimage", options={"expose"=true})
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
      */
     public function postImage(Request $request)
     {
-        dump($request->files);
+        $file = $request->files->get('file');
+        $location = $this->attachmentManager->uploadAttachment($file);
 
         return $this->json([
-            'location' => 'path/to/the/image.jpg'
+            'location' => $location
         ]);
     }
 }
--- src/Services/AttachmentManager.php
+++ src/Services/AttachmentManager.php
@@ -2,16 +2,68 @@
 
     namespace App\Services;
 
+    use Psr\Container\ContainerInterface;
+    use Symfony\Component\HttpFoundation\File\UploadedFile;
+
     class AttachmentManager
     {
 
-        public function __construct()
-        {
+        /**
+         * @var FileManager
+         */
+        private $fileManager;
+        /**
+         * @var ContainerInterface
+         */
+        private $container;
 
+        public function __construct(ContainerInterface $container, FileManager $fileManager)
+        {
+            $this->fileManager = $fileManager;
+            $this->container = $container;
         }
 
-        public function uploadAttachment()
+        /**
+         * @throws \Exception
+         */
+        public function setUploadDirectory()
         {
+            $this->fileManager->setUploadsDirectory(
+                $this->getUploadsDirectory()
+            );
+        }
 
+        /**
+         * return the full path for the attachment after uploading
+         * @param UploadedFile $file
+         * @return string
+         * @throws \Exception
+         */
+        public function uploadAttachment(UploadedFile $file)
+        {
+            $this->setUploadDirectory();
+            $filename = $this->fileManager->uploadFile($file);
+
+            return $this->getUploadsDirectoryWithoutRoot() . '/' . $filename;
+        }
+
+        /**
+         * @param string $filename
+         * @throws \Exception
+         */
+        public function removeAttachment(string $filename)
+        {
+            $this->setUploadDirectory();
+            $this->fileManager->removeFile($filename);
+        }
+
+        private function getUploadsDirectory()
+        {
+            return $this->container->getParameter('attachments_dir');
+        }
+
+        private function getUploadsDirectoryWithoutRoot()
+        {
+            return $this->container->getParameter('attachments_dir_no_root');
         }
     }
