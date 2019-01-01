04531ee9d6c0482a55097bdb2ea5963ad556c765
konshensx16 <konshensx16@gmail.com>
2019-01-01 12:09:59 +0100

    create the session table via an entity which makes easier to make it work when deploying

---
+++ src/Entity/Session.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SessionRepository")
+ * @ORM\Table(name="sessions")
+ */
+class Session
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", name="sess_id")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="blob")
+     */
+    private $sess_data;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sess_time;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sess_lifetime;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSessData()
+    {
+        return $this->sess_data;
+    }
+
+    public function setSessData($sess_data): self
+    {
+        $this->sess_data = $sess_data;
+
+        return $this;
+    }
+
+    public function getSessTime(): ?int
+    {
+        return $this->sess_time;
+    }
+
+    public function setSessTime(int $sess_time): self
+    {
+        $this->sess_time = $sess_time;
+
+        return $this;
+    }
+
+    public function getSessLifetime(): ?int
+    {
+        return $this->sess_lifetime;
+    }
+
+    public function setSessLifetime(int $sess_lifetime): self
+    {
+        $this->sess_lifetime = $sess_lifetime;
+
+        return $this;
+    }
+}
